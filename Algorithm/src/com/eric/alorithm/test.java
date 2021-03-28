886d628be3f4d677995baab7b93b900898ee54f2
Eric <zhigang_han0521@163.com>
2021-03-28 20:14:38 +0800

    auto commit

---
+++ Algorithm/src/com/eric/alorithm/test.java
@@ -0,0 +1,12 @@
+package com.eric.alorithm;
+
+/**
+ * @author：hanzhigang
+ * @Date : 2021/3/28 12:18 PM
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println((10 | 120 | ));
+        System.out.println(10 | 120);
+    }
+}
