46aeefcdf45a7abcb3b6a0c9dd7b57c1c29acbb3
Eric <zhigang_han0521@163.com>
2021-04-01 22:52:54 +0800

    auto commit

---
+++ Algorithm/src/com/eric/alorithm/leetcode/MaxArea11.java
@@ -0,0 +1,34 @@
+package com.eric.alorithm.leetcode;
+
+/**
+ * 盛最多的水
+ * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。
+ * 在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0) 。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * 说明：你不能倾斜容器。
+ *
+ * @author：hanzhigang
+ * @Date : 2021/4/1 9:12 PM
+ */
+public class MaxArea11 {
+    public int maxArea(int[] height) {
+        int max = 0;
+        int cur = 0;
+        int left = 0, right = height.length - 1;
+        while(left < right){
+            cur = Math.min(height[left],height[right]) * (right-left);
+            max = Math.max(cur,max);
+            if(height[left] > height[right]){
+                right--;
+            }else{
+                left++;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] height = new int[]{4, 3, 2, 1, 4};
+        MaxArea11 maxArea11 = new MaxArea11();
+        System.out.println(maxArea11.maxArea(height));
+    }
+}
--- Algorithm/src/com/eric/alorithm/leetcode/MyAtoi8.java
+++ Algorithm/src/com/eric/alorithm/leetcode/MyAtoi8.java
@@ -2,9 +2,9 @@ package com.eric.alorithm.leetcode;
 
 /**
  * 字符串转换整数 (atoi)
- *
+ * <p>
  * 请你来实现一个 myAtoi(string s) 函数，使其能将字符串转换成一个 32 位有符号整数（类似 C/C++ 中的 atoi 函数）。
- *
+ * <p>
  * 函数 myAtoi(string s) 的算法如下：
  * 读入字符串并丢弃无用的前导空格
  * 检查下一个字符（假设还未到字符末尾）为正还是负号，读取该字符（如果有）。 确定最终结果是负数还是正数。 如果两者都不存在，则假定结果为正。
@@ -18,6 +18,40 @@ package com.eric.alorithm.leetcode;
  */
 public class MyAtoi8 {
     public int myAtoi(String s) {
-        return 0;
+        // 返回结果
+        int result = 0;
+        int flag = 1;   // 当为负数的时候，flag = -1
+        int i = 0;  // 开始遍历的下标
+        String s1 = s.trim();   // 去掉空格
+
+        // 开头不符合规则的话返回 0
+        if (s1.length() == 0 || (s1.charAt(0) != '+' && s1.charAt(0) != '-' && s1.charAt(0) - '0' < 0 && s1.charAt(0) - '0' > 9)) {
+            return result;
+        }
+        // 判断数字的正负
+        if (s1.charAt(0) == '-') flag = -1;
+        if (s1.charAt(0) == '-' || s1.charAt(0) == '+') i = 1;
+
+        for (; i < s1.length(); i++) {
+            if (s1.charAt(i) - '0' < 0 || s1.charAt(i) - '0' > 9) break;
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && (s1.charAt(i) - '0') > Integer.MAX_VALUE % 10)) {
+                result = Integer.MAX_VALUE;
+                break;
+            }
+            if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && -(s1.charAt(i) - '0') < Integer.MIN_VALUE % 10)) {
+                result = Integer.MIN_VALUE;
+                break;
+            }
+            // 注意这里
+            result = result * 10 + flag * (s1.charAt(i) - '0');
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        MyAtoi8 myAtoi = new MyAtoi8();
+        System.out.println(myAtoi.myAtoi("-3147483648"));
+        System.out.println(Integer.MIN_VALUE);
     }
 }
