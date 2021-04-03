37fb1c182b2206ef61377b739456d236d608595c
Eric <zhigang_han0521@163.com>
2021-04-03 14:27:28 +0800

    auto commit

---
+++ JVMDemo/src/com/eric/jvmdemo/JVMMemory.java
@@ -0,0 +1,8 @@
+package com.eric.jvmdemo;
+
+/**
+ * @author：hanzhigang
+ * @Date : 2021/4/2 6:41 PM
+ */
+public class JVMMemory {
+}
---
+++ JVMDemo/src/com/eric/jvmdemo/TestCode.java
@@ -0,0 +1,36 @@
+package com.eric.jvmdemo;
+
+/**
+ * @author：hanzhigang
+ * @Date : 2021/4/2 11:08 AM
+ */
+//public class Main {
+//
+//
+//    public Main(int m) {
+//        this.m = m;
+//    }
+//
+//    private int m;
+//
+//    public int inc() {
+//        return m + 1;
+//    }
+//}
+
+public class TestCode {
+    public int foo() {
+        int x;
+        try {
+            x = 1;
+            return x;
+        } catch (Exception e) {
+            x = 2;
+            return x;
+        } finally {
+            x = 3;
+        }
+
+
+    }
+}
