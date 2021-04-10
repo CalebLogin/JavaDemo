5d71281c6228edd13b8577e000f93eaacdacbaf5
Eric <zhigang_han0521@163.com>
2021-04-10 21:09:52 +0800

    auto commit

---
+++ Java/src/com/eric/java/FailFast.java
@@ -0,0 +1,43 @@
+package com.eric.java;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * 快速错误机制
+ *
+ * @author：hanzhigang
+ * @Date : 2021/4/10 12:01 PM
+ */
+public class FailFast {
+    private static List<String> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        ThreadOne threadOne = new ThreadOne();
+        ThreadOne threadTwo = new ThreadOne();
+        new Thread(threadOne).start();
+        new Thread(threadTwo).start();
+    }
+
+    private static void printAll() {
+        String value = null;
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext()) {
+            value = (String) iterator.next();
+            System.out.println(value);
+        }
+    }
+
+    private static class ThreadOne implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                list.add(String.valueOf(i));
+                printAll();
+            }
+        }
+    }
+
+}
