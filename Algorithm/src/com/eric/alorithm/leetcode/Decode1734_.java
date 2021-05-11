b067105695ff66919a9585fcfe474d69557b89d6
Eric <zhigang_han0521@163.com>
2021-05-11 09:27:35 +0800

    update 新增题解
    岛屿数量

---
+++ Algorithm/src/com/eric/alorithm/leetcode/Decode1734_.java
@@ -0,0 +1,18 @@
+package com.eric.alorithm.leetcode;
+
+/**
+ * 解码异或后的排列
+ * @author：hanzhigang
+ * @Date : 2021/5/11 12:04 AM
+ */
+public class Decode1734_ {
+    public int[] decode(int[] encoded) {
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(6 ^ 5);
+        System.out.println(5 ^ 4);
+        System.out.println(4 ^ 6);
+    }
+}
