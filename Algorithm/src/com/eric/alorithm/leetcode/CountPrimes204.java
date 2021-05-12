c994ea50fbf85d3295a26119975ead1caf5b0249
Eric <zhigang_han0521@163.com>
2021-05-13 00:19:44 +0800

    update: 新增新的题解
    新增了快乐数的题解

---
+++ Algorithm/src/com/eric/alorithm/leetcode/CountPrimes204.java
@@ -0,0 +1,14 @@
+package com.eric.alorithm.leetcode;
+
+/**
+ * 计数质数
+ * 统计所有小于非负整数 n 的质数的数量。
+ * @author：hanzhigang
+ * @Date : 2021/5/13 12:18 AM
+ */
+public class CountPrimes204 {
+
+    public int countPrimes(int n) {
+        return 0;
+    }
+}
