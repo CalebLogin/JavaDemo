00095d5128eb467c3d3d4815889cc7009d1f18f4
Eric <zhigang_han0521@163.com>
2021-05-25 13:23:02 +0800

    auto commit

---
+++ Algorithm/src/com/caleb/alorithm/leetcode/DeleteNode237.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @Author:   Caleb
+ * @DateTime: 2021-05-25 13:17:48
+ * ============================
+ */
+
+public class DeleteNode237{
+	public static void main(String[] args) {
+		System.out.println("hhhhhh");
+	}
+}
