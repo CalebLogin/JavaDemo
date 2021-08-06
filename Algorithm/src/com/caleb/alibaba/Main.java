f5329e5ce14d5839e49d67982a54782f071dc285
CalebLogin <zhigang_han0521@163.com>
2021-08-06 23:02:52 +0800

    auto commit

---
+++ Algorithm/src/com/caleb/alibaba/Main.java
@@ -0,0 +1,86 @@
+package com.caleb.alibaba;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * @author:Caleb
+ * @Date :2021-08-06 19:09:52
+ */
+public class Main {
+
+	public static void newWorld() {
+		Scanner sc = new Scanner(System.in);
+		int d = sc.nextInt();
+		int m = sc.nextInt();
+		int w = sc.nextInt();
+		int i = sc.nextInt();
+		int j = sc.nextInt();
+		int k = sc.nextInt();
+		sc.close();
+		long dayCount = (k - 1) * m * d;
+		dayCount += (j - 1) * d;
+		dayCount += i;
+		dayCount -= 1;
+		dayCount %= w;
+		char dayC = (char) ('a' + dayCount);
+		System.out.println(dayC);
+	}
+
+	public static void niuniu() {
+		List<Set<Integer>> leagues = new ArrayList<>();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			int t = sc.nextInt();
+			if(t == 2){
+				int a = sc.nextInt();
+				int b = sc.nextInt();
+				int c = sc.nextInt();
+				int num1 = a % c;
+				int num2 = b % c;
+				if(num1 == num2){
+					System.out.println("YES");
+				}else{
+					boolean flag = false;
+					for(Set<Integer> set : leagues){
+						if(set.contains(num1) && set.contains(num2)){
+							flag = true;
+							System.out.println("YES");
+							break;
+						}
+					}
+					if(!flag){
+						System.out.println("NO");
+					}	
+				}
+			}else{
+				int a = sc.nextInt();
+				int b = sc.nextInt();
+				boolean flag = false;
+				for(Set<Integer> set : leagues){
+					if(set.contains(a) || set.contains(b)){
+						flag = true;
+						set.add(a);
+						set.add(b);
+						break;
+					}
+				}
+				if(!flag){
+					Set<Integer> newSet = new HashSet<>();
+					newSet.add(a);
+					newSet.add(b);
+					leagues.add(newSet);
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		niuniu();
+	}
+
+}
---
+++ Algorithm/src/com/caleb/datastructure/QuickUnion.java
@@ -0,0 +1,43 @@
+package com.caleb.datastructure;
+
+public class QuickUnion {
+
+	int[] root;
+	int[] rank;
+
+	public QuickUnion(int size) {
+		root = new int[size];
+		rank = new int[size];
+		for (int i = 0; i < size; i++) {
+			root[i] = -1;
+			rank[i] = 1;
+		}
+	}
+
+	public int find(int x) {
+		if (root[x] == x) {
+			return x;
+		}
+		return root[x] = find(root[x]);
+	}
+
+	public void union(int x, int y) {
+		int xRoot = find(x);
+		int yRoot = find(y);
+		if (xRoot != yRoot) {
+			if (rank[xRoot] > rank[yRoot]) {
+				root[yRoot] = xRoot;
+			} else if (rank[xRoot] < rank[yRoot]) {
+				root[xRoot] = yRoot;
+			} else {
+				root[xRoot] = yRoot;
+				rank[yRoot]++;
+			}
+
+		}
+	}
+
+	public boolean connected(int x, int y) {
+		return find(x) == find(y);
+	}
+}
